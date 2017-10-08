049a35a4bfee61d66d0f1d5585110a8fdceaf18e
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2017-10-09 03:12:45 +0530

    Added subprotocol support and introduced delay in echo

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"time"
-
-	"net"
-
 	"encoding/base64"
+	"flag"
 	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -17,24 +17,40 @@ const (
 	pongTimeout = 60 * time.Second
 )
 
-var addr = flag.String("addr", "", "http service address")
+var addrFlag = flag.String("addr", "ws://echo.websocket.org", "http service address")
+var subProtocolFlag = flag.String("protocols", "", "Comma separated list of protocols to use")
+var echoDelayFlag = flag.Uint("echoDelay", 0, "Delay before echoing back received message from server")
 
 func main() {
 	flag.Parse()
 
-	if *addr == "" {
-		flag.Usage()
-		log.Fatalf("Server address \"%s\" is invalid.", *addr)
+	var address string = *addrFlag
+	var subProtocols []string
+	var echoDelay = *echoDelayFlag
+
+	if address == "" {
+		log.Fatalf("Server address \"%s\" is invalid.", address)
 	}
 
-	var dialer *websocket.Dialer
+	if *subProtocolFlag != "" {
+		subProtocols = strings.Split(*subProtocolFlag, ",")
+		log.Printf("Using sub protocols: %s\n", strings.Join(subProtocols, ", "))
+	}
 
-	conn, _, err := dialer.Dial(*addr, nil)
+	if echoDelay < 0 {
+		log.Fatalln("Echo delay cannot be negative")
+	}
+
+	var dialer *websocket.Dialer = &websocket.Dialer{
+		Subprotocols: subProtocols,
+	}
+
+	conn, _, err := dialer.Dial(address, nil)
 	if err != nil {
-		log.Fatalf("Unable to dial ws connection to agent %s with error %s\n", *addr, err)
+		log.Fatalf("Unable to dial ws connection to agent %s with error %s\n", address, err)
 	}
 
-	log.Println("Connection stablized")
+	log.Println("Connection stabilized")
 	defer conn.Close()
 
 	conn.SetCloseHandler(func(code int, text string) error {
@@ -64,6 +80,8 @@ func main() {
 		},
 	)
 
+	var wg sync.WaitGroup
+
 	for {
 		msgType, rawMsg, err := conn.ReadMessage()
 		if err != nil {
@@ -80,13 +98,21 @@ func main() {
 			log.Printf("Received unknown message type: %d.\n Base64 of message: %s\n", msgType, base64.StdEncoding.EncodeToString(rawMsg))
 		}
 
-		err = conn.WriteMessage(msgType, rawMsg)
-		if err == nil {
-			log.Printf("Sent received message back to server")
-		} else {
-			log.Printf("Unable to send message to server. Error: %s\n", err)
-		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			time.Sleep(time.Duration(time.Second * time.Duration(echoDelay)))
+
+			err = conn.WriteMessage(msgType, rawMsg)
+			if err == nil {
+				log.Printf("Sent received message back to server")
+			} else {
+				log.Printf("Unable to send message to server. Error: %s\n", err)
+			}
+		}()
 	}
+
+	wg.Wait()
 
 	log.Println("Exiting...")
 }
